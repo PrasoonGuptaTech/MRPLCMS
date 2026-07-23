5b57615bc00033a201b616068177697eb0d040a1
Prasoon Gupta <ishankgupta1may@gmail.com>
2026-07-23 10:31:18 +0530

    feat: firebase crashlytics

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { checkForUpdate } from './source/feature/forceUpdate/services/remoteConfig';
 import { compareVersions } from './source/feature/forceUpdate/utility/Utility';
+import {
+  crash,
+  getCrashlytics,
+  log,
+  setAttributes,
+  setUserId,
+} from '@react-native-firebase/crashlytics';
 
 function App() {
   const [showForceUpdateModal, setShowForceUpdateModal] = useState(false);
@@ -20,10 +27,28 @@ function App() {
     }
   };
   console.log(showForceUpdateModal);
+  useEffect(() => {
+    log(getCrashlytics(), 'App Mounted');
+  }, []);
+  const forceCrash = async () => {
+    const crashlytics = getCrashlytics();
+    log(crashlytics, 'User Tapped Force Crash');
+    await Promise.all([
+      setUserId(crashlytics, 'Aa0Bb1Cc2Dd3Ee4Ff5Gg6Hh7Ii8Jj9'),
+      setAttributes(crashlytics, {
+        role: 'admin',
+        email: 'ishankgupta1may@gmail.com',
+        username: 'Prasoon Gupta',
+      }),
+    ]);
+    crash(crashlytics);
+  };
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
         <Text>App</Text>
+        <Button title="Force Crash" onPress={forceCrash} />
+        <Button title="Test Crash" onPress={() => crash(getCrashlytics())} />
         {showForceUpdateModal && <Text>Force Update Message Required</Text>}
       </View>
     </SafeAreaProvider>
